fb234c44a8f4ba3bcc553929786029640d20fb23
代震 <daizhen256@sina.com>
2018-05-06 21:43:12 +0800

    init

---
+++ src/test/java/com/daizhen/gasmine/GasmineStandaloneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daizhen.gasmine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GasmineStandaloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
